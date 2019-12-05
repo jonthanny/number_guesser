3f2829329dd2ca62ffded8b031e1c44ff02a5bb1
Erik Raith <raithhtiar@gmail.com>
2019-12-05 12:47:18 -0700

    Seperated functions for Unit Testing

--- main.js
+++ main.js
@@ -4,23 +4,36 @@ const submitButton = document.querySelector("#submit-button");
 const resetButton = document.querySelector("#reset-button");
 const clearFormButton = document.querySelector("#clear-form-button");
 
-// checkComplete() is called whenever a input field is changed.
+// checkFormInputs() is called whenever a input field is changed.
 // This enables and disables the submitButton and clearFormButton variables
-function checkComplete() {
-  //Starts as true. Checks
-  var canSubmit = true;
-  var canClear = false;
+function checkFormInputs() {
+
   //Loop through inputs[] check to see if anything is blank.
   //When canClear is false button appears
+  submitButtonEnable();
+  clearFormButtonEnable();
+
+}
+
+function submitButtonEnable(){
+  //Starts as true. Checks
+  var canSubmit = true;
   for (var i = 0; i < inputs.length; i++) {
     if (inputs[i].value.length == 0) {
       canSubmit = false;
-    }else if (canClear == false) {
+    }
+  }
+  submitButton.disabled = !canSubmit;
+}
+
+function clearFormButtonEnable(){
+  var canClear = false;
+  for (var i = 0; i < inputs.length; i++) {
+    if (inputs[i].value.length != 0 && canClear==false) {
       canClear = true;
     }
-    submitButton.disabled = !canSubmit;
-    clearFormButton.disabled = !canClear;
   }
+  clearFormButton.disabled = !canClear;
 }
 //Clears the inputs from the form
 function clearForm() {
@@ -30,9 +43,10 @@ function clearForm() {
 }
 
 //Event Listeners
-//array.forEach(function(input){input.addEventListener("focusout",checkComplete)}
-inputs.forEach((input) => addEventListener("input", checkComplete));
+//array.forEach(function(input){input.addEventListener("focusout",checkFormInputs)}
+inputs.forEach((input) => addEventListener("input", checkFormInputs));
 clearFormButton.addEventListener("click", clearForm);
 
 
-window.onload = checkComplete();
+window.onload = checkFormInputs();
+// module.exports = Card;
