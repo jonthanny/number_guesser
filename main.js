722ad02c257e859358addf9fdb7bf41080059dcf
Erik Raith <raithhtiar@gmail.com>
2019-12-04 22:44:36 -0700

    WIP change disable to enable. Then back

--- main.js
+++ main.js
@@ -7,12 +7,11 @@ const clearFormButton = document.querySelector("#clear-form-button");
 // checkComplete() is called whenever a input field is changed.
 // This enables and disables the submitButton and clearFormButton variables
 function checkComplete() {
+  //Starts as true. Checks
   var canSubmit = true;
   var canClear = false;
   //Loop through inputs[] check to see if anything is blank.
   //When canClear is false button appears
-
-  //[asd,asdf,asdf,asdf]
   for (var i = 0; i < inputs.length; i++) {
     if (inputs[i].value.length == 0) {
       canSubmit = false;
